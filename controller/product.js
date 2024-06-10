7b67eb95d700e7ef4c5a89a178822fd570e8a979
ductain <taindse170080@fpt.edu.vn>
2024-06-10 16:46:58 +0700

    random product

--- controller/product.js
+++ controller/product.js
@@ -35,7 +35,27 @@ const getProductById = async (req, res) => {
   }
 };
 
+const getFiveRandomProductsDaily = async (req, res) => {
+  try {
+    const pool = await sql.connect(config);
+    const dateSeed = new Date().setHours(0, 0, 0, 0); // Reset time to midnight for daily seed
+    const products = await pool.request().query(
+      `SELECT TOP 5 PRODUCT_ID, PRODUCTNAME, ORIGIN, PRICE, PRODUCTDETAILS, PRODUCTMATERIAL, QUANTITY, PRODUCTIMAGE, PR.TYPE_ID, T.TYPENAME, T.RATIO, T.HEIGHT, PR.BRAND_ID, BR.BRANDNAME
+         FROM PRODUCT AS PR
+         JOIN TYPES AS T ON PR.TYPE_ID = T.TYPE_ID
+         JOIN BRAND AS BR ON PR.BRAND_ID = BR.BRAND_ID
+         ORDER BY CHECKSUM(NEWID(), ${dateSeed})`
+    );
+    res.status(200).json(products.recordset);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error fetching random products", error: error });
+  }
+};
+
 module.exports = {
   getProducts: getProducts,
   getProductById: getProductById,
+  getFiveRandomProductsDaily: getFiveRandomProductsDaily,
 };
